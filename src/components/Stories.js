c095392b071bc75674cbfeb8773889aab74842ae
Luiz Fernando <lfaires@gmail.com>
2021-04-30 11:20:36 -0300

    componentizacao das sugestoes finaliada

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -22,6 +22,5 @@ function Story (props) {
             {props.nome}
         </div>
     </div>
-     
     )
 }
--- src/components/Sugestoes.js
+++ src/components/Sugestoes.js
@@ -1,71 +1,24 @@
+const user = ["bad.vibes.memes" ,"chibirdart" ,"razoesparaacreditar" ,"adorable_animals" ,"smallcutecats"]
+
 export default function Sugestoes() {
     return (
-        <div class="sugestoes">
-          <div class="titulo">
-            Sugestões para você
-            <div>Ver tudo</div>
-          </div>
-
-          <Sugestao />
-
-          <div class="sugestao">
-            <div class="usuario">
-              <img src="assets/img/chibirdart.svg" />
-              <div class="texto">
-                <div class="nome">chibirdart</div>
-                <div class="razao">Segue você</div>
-              </div>
-            </div>
-
-            <div class="seguir">Seguir</div>
-          </div>
-
-          <div class="sugestao">
-            <div class="usuario">
-              <img src="assets/img/razoesparaacreditar.svg" />
-              <div class="texto">
-                <div class="nome">razoesparaacreditar</div>
-                <div class="razao">Novo no Instagram</div>
-              </div>
-            </div>
-
-            <div class="seguir">Seguir</div>
-          </div>
-
-          <div class="sugestao">
-            <div class="usuario">
-              <img src="assets/img/adorable_animals.svg" />
-              <div class="texto">
-                <div class="nome">adorable_animals</div>
-                <div class="razao">Segue você</div>
-              </div>
-            </div>
-
-            <div class="seguir">Seguir</div>
-          </div>
-
-          <div class="sugestao">
-            <div class="usuario">
-              <img src="assets/img/smallcutecats.svg" />
-              <div class="texto">
-                <div class="nome">smallcutecats</div>
-                <div class="razao">Segue você</div>
-              </div>
-            </div>
-
-            <div class="seguir">Seguir</div>
-          </div>
+      <div class="sugestoes">
+        <div class="titulo">
+          Sugestões para você
+          <div>Ver tudo</div>
         </div>
+        {user.map( nome => <Sugestao nome={nome} />)}
+      </div>
     )
 }
 
-function Sugestao() {
+function Sugestao(props) {
     return (
         <div class="sugestao">
             <div class="usuario">
-                <img src="assets/img/bad.vibes.memes.svg" />
+                <img src={"assets/img/"+ props.nome +".svg"} />
                 <div class="texto">
-                <div class="nome">bad.vibes.memes</div>
+                <div class="nome">{props.nome}</div>
                 <div class="razao">Segue você</div>
                 </div>
             </div>
